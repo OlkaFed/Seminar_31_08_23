6838800e026ff20e28954c43c89864ee942cebd7
Olya <Olya.fedorowskaya@yandex.ru>
2023-08-31 21:18:46 +0300

    Задача на день недели по числу

---
+++ Seminar3/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.Write("Enter number: ");
+string strNum = Console.ReadLine();
+int WeekDay = int.Parse(strNum); 
+
+if(WeekDay==1) 
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if(WeekDay==2) 
+{
+    Console.WriteLine("Вторник");
+}
+
+else if(WeekDay==3) 
+{
+    Console.WriteLine("Среда");
+}
+
+else if(WeekDay==4) 
+{
+    Console.WriteLine("Четверг");
+}
+
+else if(WeekDay==5) 
+{
+    Console.WriteLine("Пятница");
+}
+
+else if(WeekDay==6) 
+{
+    Console.WriteLine("Суббота");
+}
+
+else if(WeekDay==7) 
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else 
+{
+   Console.WriteLine("Вы ввели некорректное число"); 
+}
