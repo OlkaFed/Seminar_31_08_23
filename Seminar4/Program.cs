999f425ab55c0106b513bc504850fefe5aaf7051
Olya <Olya.fedorowskaya@yandex.ru>
2023-08-31 21:44:24 +0300

    Массив чисел

---
+++ Seminar4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//2 -> " -2, -1, 0, 1, 2" 
+
+Console.WriteLine("ВВедите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+int num0 = num * -1;
+
+while (num0<=num)
+{
+Console.Write(num0);
+num0++;
+}
