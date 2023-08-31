96fb1d7c17492af878b2cd9f1dd794bf334ab037
Olya <Olya.fedorowskaya@yandex.ru>
2023-08-31 20:45:41 +0300

    Задача на нахождение квадрата числа

---
+++ Seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+
+Console.Write("Введите число ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+
+int sqr = num * num;
+
+Console.WriteLine(sqr); 
